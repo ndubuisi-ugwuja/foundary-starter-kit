b4357b52dae12fea17db49af35d3958628c446f5
Ndubuisi <ndujiovita1@gmail.com>
2026-01-08 07:52:03 +0100

    add deploy script

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.13;
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
-contract DeploySimpleStorage is Script {}
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+} 
